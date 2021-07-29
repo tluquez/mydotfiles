fa613029fd89df19db86daa278018ef857851359
tain <tain@pilot.local>
2021-07-29 12:35:32 -0500

    Wrapping up initial set up 2

--- SetUpLinuxOnRemote.sh
+++ SetUpLinuxOnRemote.sh
@@ -2,50 +2,58 @@
 
 set -u -o pipefail
 
+# Set up variables
 CONDA=${1:-no}
 
-printf "\nLet's begging setting up your bash environment baby!!!\n\n"
-printf "**** Remember this script overwrites any previous files or symlinks ****\n\n"
+# Let's rock it!
+printf "\nLet's begging setting up your bash environment baby!!!\n"
+printf "**** Remember this script overwrites any previous files or symlinks ****\n"
 
 # Bash version
-if [[ `echo "${BASH_VERSINFO:-0}"'<='3.9 | bc -l` ]]; then
+if [[ `echo "${BASH_VERSINFO:-0}"'<='3.9 | bc -l` == 1 ]]; then
   echo -e "Upgrading bash\n"
   sudo apt-get install --only-upgrade bash
 fi
 
 # Git clone mydotfiles
-git clone https://github.com/tluquez/mydotfiles.git $HOME
+MYDOTFILES="$HOME/mydotfiles"
+if [[ ! -d $MYDOTFILES ]]; then
+  git clone https://github.com/tluquez/mydotfiles.git $MYDOTFILES
+  else
+  rm -rf $MYDOTFILES
+  git clone https://github.com/tluquez/mydotfiles.git $MYDOTFILES
+fi
 
 # Symlink dot files
-ln -sfv $HOME/mydotfiles/my.bashrc_linux $HOME/.bashrc
-ln -sfv $HOME/mydotfiles/my.bash_profile $HOME/.bash_profile
-ln -sfv $HOME/mydotfiles/my.bash_prompt $HOME/.bash_prompt
-ln -sfv $HOME/mydotfiles/my.bash_aliases $HOME/.bash_aliases
-ln -sfv $HOME/mydotfiles/my.bash_functions $HOME/.bash_functions
-#ln -sfv $HOME/mydotfiles/my.private_info $HOME/.private_info
-ln -sfv $HOME/mydotfiles/my.inputrc $HOME/.inputrc
+ln -sfv $MYDOTFILES/my.bashrc_linux $HOME/.bashrc
+ln -sfv $MYDOTFILES/my.bash_profile $HOME/.bash_profile
+ln -sfv $MYDOTFILES/my.bash_prompt $HOME/.bash_prompt
+ln -sfv $MYDOTFILES/my.bash_aliases $HOME/.bash_aliases
+ln -sfv $MYDOTFILES/my.bash_functions $HOME/.bash_functions
+#ln -sfv $MYDOTFILES/my.private_info $HOME/.private_info
+ln -sfv $MYDOTFILES/my.inputrc $HOME/.inputrc
 if [[ ! -d $HOME/.tmux.conf ]]; then
   mkdir -p $HOME/.config/htop/
-  ln -sfv $HOME/mydotfiles/my.htoprc $HOME/.config/htop/htoprc
+  ln -sfv $MYDOTFILES/my.htoprc $HOME/.config/htop/htoprc
   else
-  ln -sfv $HOME/mydotfiles/my.htoprc $HOME/.config/htop/htoprc
+  ln -sfv $MYDOTFILES/my.htoprc $HOME/.config/htop/htoprc
 fi
 if [[ ! -d $HOME/.vim/colors/ ]]; then
   mkdir -p $HOME/.vim/colors/
-  ln -sfv $HOME/mydotfiles/my.vimrc_monokai-phenix.vim $HOME/.vim/colors/monokai-phoenix.vim
+  ln -sfv $MYDOTFILES/my.vimrc_monokai-phenix.vim $HOME/.vim/colors/monokai-phoenix.vim
   else
-    ln -sfv $HOME/mydotfiles/my.vimrc_monokai-phenix.vim $HOME/.vim/colors/monokai-phoenix.vim
+    ln -sfv $MYDOTFILES/my.vimrc_monokai-phenix.vim $HOME/.vim/colors/monokai-phoenix.vim
 fi
-ln -sfv $HOME/mydotfiles/my.vimrc $HOME/.vimrc
+ln -sfv $MYDOTFILES/my.vimrc $HOME/.vimrc
 
 #Set up .tmux.conf depending on the version
 if [[ ! -f $HOME/.tmux.conf ]]; then
   if [[ `tmux -V` < 3  ]]; then
     printf "\nGetting .tmux.conf version 2\n"
-    ln -sfv $HOME/mydotfiles/.tmux.conf.V2 $HOME/.tmux.conf
+    ln -sfv $MYDOTFILES/.tmux.conf.V2 $HOME/.tmux.conf
   else
     printf "\nGetting .tmux.conf version 3\n"
-    ln -sfv $HOME/mydotfiles/.tmux.conf.V3 $HOME/.tmux.conf
+    ln -sfv $MYDOTFILES/.tmux.conf.V3 $HOME/.tmux.conf
   fi
 fi
 
@@ -54,7 +62,7 @@ touch $HOME/.hushlogin
 
 printf "\nSourcing and binding\n\n"
 . $HOME/.bashrc
-bind -f  $HOME/.inputrc
+#bind -f  $HOME/.inputrc
 
 # Check if mybin exists if not create it
 if [[ ! -d "$HOME/mybin" ]]; then
@@ -80,5 +88,5 @@ if [[ ${CONDA} == "conda" ]]; then
   parallel --cite
 fi
 
-printf "All done!\n See you later, alligator!\n"
+printf "All done! It's a good idea to log out and back in to enable .inputrc.\n See you later, alligator!\n"
 exit 0
